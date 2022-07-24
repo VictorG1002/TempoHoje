70d9bace45b68c7b38346882b4b390a49d89d291
VictorG1002 <vggaia10@gmail.com>
2022-07-24 14:38:49 -0300

    add api axios

---
+++ src/services/api.ts
@@ -0,0 +1,13 @@
+import axios from 'axios'
+
+const api = axios.create({
+  baseURL: 'http://apiadvisor.climatempo.com.br',
+  headers: {
+    'Access-Control-Allow-Origin': ''
+  }
+})
+
+api.defaults.headers.common['Authorization'] =
+  '1e4c111c74cbbfef85a7c61edc644ded'
+
+export default api
