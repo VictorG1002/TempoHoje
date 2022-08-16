08a1dc41909107a3a4d639e958f2ccd88fc1146d
VictorG1002 <vggaia10@gmail.com>
2022-08-15 21:59:59 -0300

    edit layout

--- src/Styles/index.tsx
+++ src/Styles/index.tsx
@@ -4,7 +4,12 @@ const customTheme = extendTheme({
   fonts: {
     body: 'Poppins, sans-serif',
     heading: 'Poppins, sans-serif'
-  }
+  },
+  global: () => ({
+    'html, body, #root': {
+      minHeight: '100%'
+    }
+  })
 })
 
 export default customTheme
